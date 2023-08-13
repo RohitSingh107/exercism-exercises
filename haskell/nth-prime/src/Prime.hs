88d2e9a52f6c39d1924cd906d70bd8e3139828c5
Rohit Singh <RohitSinghEmail@protonmail.com>
2023-08-13 20:53:28 +0530

    prime nums

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,15 @@
 module Prime (nth) where
 
+sieve' :: [Integer] -> [Integer]
+sieve' [] = []
+sieve' (x:xs) = x : sieve' [y | y <- xs, y `rem` x /= 0]
+-- sieve' (x:xs) = x : sieve'  (filter (\y -> y `rem` x /= 0) xs) -- Correct
+
+sieve :: [Integer]
+sieve = sieve' [2..]
+
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+-- nth n = Just ((!!) sieve (n-1))
+nth n
+  | n <= 0 = Nothing
+  | otherwise = Just ((!!) sieve (n-1))
