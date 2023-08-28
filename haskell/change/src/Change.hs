787fd82068fe17fea7f739ea691a5084c2e6a547
Rohit Singh <RohitSinghEmail@protonmail.com>
2023-08-28 21:58:12 +0530

    more exerices

--- haskell/change/src/Change.hs
+++ haskell/change/src/Change.hs
@@ -1,22 +1,27 @@
 module Change (findFewestCoins) where
 
-{-
-findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
-findFewestCoins target coins = if head coins > target then Nothing else Just (sol target (n-1) [])
-  where
-    n = length coins
-    sol :: Integer -> Int -> [Integer] -> [Integer]
-    sol 0 _ ans = ans
-    sol t i ans = if t >= (coins !! i) then sol (t `mod` (coins !! i)) i ((coins !! i) : ans)  else sol t (i-1) ans
--}
+import Data.Vector (Vector, (!), fromList)
+
+
 
 
 findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
-findFewestCoins target coins = undefined
+findFewestCoins target coins = case findFewestCoins' target (length coins -1) (fromList coins) of
+                                 Nothing -> Nothing
+                                 Just xs -> Just (reverse xs)
 
 
-findFewestCoins' :: Integer -> [Integer] -> [Integer] -> Maybe [Integer]
-findFewestCoins' target coins ans
-  | target == 0 = Just ans
-  | target < 0 = Nothing
-  | otherwise = undefined
+findFewestCoins' :: Integer -> Int -> Vector Integer -> Maybe [Integer]
+findFewestCoins' target c coins
+  | target == 0 = Just []
+  | target < 0 || c < 0 = Nothing
+  | otherwise = case (take', not_take) of
+                  (Nothing, Nothing) -> Nothing
+                  (Nothing, Just xs) -> Just xs
+                  (Just xs, Nothing) -> Just (coin : xs)
+                  (Just xs, Just ys) -> if length xs + 1 < length ys then Just (coin : xs) else Just ys
+    where
+      coin = coins ! c
+      take' = findFewestCoins' (target - coin) c coins
+      not_take = findFewestCoins' target (c-1) coins
+
